1b86510e4cf6fbf18d6b0e1f5f50677d73b6177f
Victor Duque <victor.duque11@hotmail.com>
2023-03-22 17:38:58 -0300

    agora foi

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,13 +2,16 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async (param) => {
+export const fetchProductsList = async ($QUERY) => {
   // seu código aqui
   try {
-    if (!param) {
+    if (!$QUERY) {
       throw new Error('Termo de busca não informado');
     }
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
+    const promisse = await fetch(url);
+    const retorno = await promisse.json().then((result) => result.results);
+    return retorno;
   } catch (e) {
     return e;
   }
