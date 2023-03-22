cdf70055ac40ce528c4ebab1474a817a5936090e
Victor Duque <victor.duque11@hotmail.com>
2023-03-22 15:46:48 -0300

    requisito 1 feito (eu acho) e parte do 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,14 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async (param) => {
   // seu código aqui
+  try {
+    if (!param) {
+      throw new Error('Termo de busca não informado');
+    }
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+  } catch (e) {
+    return e;
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    fetchProductsList('computador');
+    await expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    fetchProductsList('computador');
+    await expect(fetch).toHaveBeenCalledWith(url);
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const arg = await fetchProductsList('computador');
+    expect(arg).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro.', async () => {
+    const arg = await fetchProductsList();
+    expect(arg).toEqual(new Error('Termo de busca não informado'));
+  });
 });
