20551007199ed007192e4e8a53c883141b37d5ec
Victor Duque <victor.duque11@hotmail.com>
2023-03-23 17:12:16 -0300

    requisito 4 e parte do 5 feitos

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
@@ -19,25 +20,44 @@ function carregou() {
   span.remove();
 }
 
+// function nemCarregou(error) {
+//   const span = document.querySelector('.loading');
+//   console.log('erro recebido em nemCarregou', error);
+//   span.innerHTML = `${error}`;
+// }
+
 const mostrarProdutos = async () => {
   try {
     const request = await fetchProductsList('computador');
+    // .then((response) => {
+    if (!request) {
+      throw new Error('Algum erro ocorreu, recarregue a página e tente novamente');
+    }
     request.forEach((product) => {
       prodSection.appendChild(createProductElement(product));
     });
+    // });
     carregou();
-  } catch (e) {
-    console.log(e.message);
-    throw new Error('Error...');
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${error}`,
+    });
+    // console.log(e.message);
+    // const erro = e.message;
+    // const span = document.querySelector('.loading');
+    // span.innerHTML = `${erro}`;
+    // throw new Error('Algum erro ocorreu, recarregue a página e tente novamente');
   }
 };
 
-const btnAdd = document.querySelectorAll('.product_add')[index];
-btnAdd.addEventListener('click', addCarrinho);
+// const btnAdd = document.querySelectorAll('.product_add')[index];
+// btnAdd.addEventListener('click', addCarrinho);
 
-const addCarrinho = async (idDoProduto) => {
-  const id = await idDoProduto.target.parentNode.firstChild.innerText;
-};
+// const addCarrinho = async (idDoProduto) => {
+//   const id = await idDoProduto.target.parentNode.firstChild.innerText;
+// };
 
 window.onload = function onload() {
   carregando();
