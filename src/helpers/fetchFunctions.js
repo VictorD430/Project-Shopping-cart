083fe7211e8f2a41ed9931f9da7549f02584704a
Victor Duque <victor.duque11@hotmail.com>
2023-03-23 00:18:02 -0300

    um pouquinho de cada coisa

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,18 +1,33 @@
-export const fetchProduct = () => {
+export const fetchProduct = async ($ProductID) => {
   // seu código aqui
+  // try {
+  //   const vazio = 'Nenhum item para ser adicionado';
+  //   if ($QUERY === '') {
+  //     throw new Error(vazio);
+  //   }
+  //   const url = `https://api.mercadolibre.com/items/${$ProductID}`;
+  //   const promisse = await fetch(url);
+  //   const retorno = await promisse.json();
+  //   return retorno.results;
+  // } catch (e) {
+  //   console.log('catch', e.message);
+  //   throw new Error('Nada foi adicionado');
+  // }
 };
 
 export const fetchProductsList = async ($QUERY) => {
   // seu código aqui
   try {
-    if (!$QUERY) {
-      throw new Error('Termo de busca não informado');
-    }
+    const vazio = 'Termo de busca não informado';
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
     const promisse = await fetch(url);
     const retorno = await promisse.json();
+    if ($QUERY === '' || $QUERY === retorno.length) {
+      throw new Error(vazio);
+    }
     return retorno.results;
   } catch (e) {
-    return e;
+    console.log('catch', e.message);
+    throw new Error('Termo de busca não informado');
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-// iniciando
+const prodSection = document.querySelector('.products');
+
+function carregando() {
+  const carregaSection = document.querySelector('.products');
+  const span = document.createElement('span');
+  span.classList = 'loading';
+  span.innerHTML = 'carregando...';
+  carregaSection.appendChild(span);
+  span.remove();
+}
+
+const mostrarProdutos = async () => {
+  try {
+    const request = await fetchProductsList('computador');
+    request.forEach((product) => {
+      prodSection.appendChild(createProductElement(product));
+      // carregou();
+    });
+  } catch (e) {
+    console.log(e.message);
+    throw new Error('Error...');
+  }
+};
+
+window.onload = function onload() {
+  carregando();
+  mostrarProdutos();
+};
