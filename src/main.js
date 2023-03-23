85dc38830d61b574291b35b043b021ab7b8056fb
Victor Duque <victor.duque11@hotmail.com>
2023-03-23 00:52:10 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ function carregando() {
   span.classList = 'loading';
   span.innerHTML = 'carregando...';
   carregaSection.appendChild(span);
+}
+
+function carregou() {
+  const span = document.querySelector('.loading');
   span.remove();
 }
 
@@ -20,12 +24,19 @@ const mostrarProdutos = async () => {
     const request = await fetchProductsList('computador');
     request.forEach((product) => {
       prodSection.appendChild(createProductElement(product));
-      // carregou();
     });
+    carregou();
   } catch (e) {
     console.log(e.message);
     throw new Error('Error...');
   }
+};
+
+const btnAdd = document.querySelectorAll('.product_add')[index];
+btnAdd.addEventListener('click', addCarrinho);
+
+const addCarrinho = async (idDoProduto) => {
+  const id = await idDoProduto.target.parentNode.firstChild.innerText;
 };
 
 window.onload = function onload() {
