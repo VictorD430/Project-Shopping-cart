2f796575d93d9393452808a612af4a1a2baf19f4
Victor Duque <victor.duque11@hotmail.com>
2023-03-23 22:29:42 -0300

    requisito 11 feito

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,21 +1,25 @@
-export const getAddress = ($CEP) => {
-  // seu código aqui
+export const getAddress = (param) => {
+  const { address, street, district, neighborhood, city, state } = param;
   const span = document.querySelector('.cart__address');
-  const url1 = `https://cep.awesomeapi.com.br/json/${$CEP}`;
-  const url2 = `https://brasilapi.com.br/api/cep/v2/${$CEP}`;
-  const pSlow = new Promise((resolve, reject) => {
-    setTimeout(resolve, 500, "Done eventually");
-  });
-  const pFast = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, "Done quick");
-  });
-  Promise.any([pSlow, pFast]).then((value) => {
-    console.log(value);
-  });
+  if (Object.prototype.hasOwnProperty.call(param, 'city')) {
+    if (Object.prototype.hasOwnProperty.call(param, 'address')) {
+      span.innerText = `${address} - ${district} - ${city} - ${state}`;
+    } else {
+      span.innerText = `${street} - ${neighborhood} - ${city} - ${state}`;
+    }
+  } else {
+    throw new Error('CEP não encontrado');
+  }
 };
 
-export const searchCep = () => {
-  // seu código aqui
-  const textArea = document.querySelector('.cep-input');
-  const btnSearch = docuent.querySelector('cep-button');
+export const searchCep = async () => {
+  const span = document.querySelector('.cart__address');
+  const $CEP = document.querySelector('.cep-input').value;
+  const api1 = await fetch(`https://brasilapi.com.br/api/cep/v2/${$CEP}`);
+  const api2 = await fetch(`https://cep.awesomeapi.com.br/json/${$CEP}`);
+  const promises = [api1, api2];
+  Promise.any(promises).then((result) => result.json())
+    .then((retorno) => getAddress(retorno))
+    .catch((error) => console.log(error.message))
+    .then(span.innerText = 'CEP não encontrado');
 };
